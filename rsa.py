d3cb26f3607b1647320199b80c8219c37d347f78
Sidharth Rao <sidharthmrao@gmail.com>
2022-01-16 18:14:01 -0500

    WORKS WITH NUMBERS!!!

--- rsa.py
+++ rsa.py
@@ -3,9 +3,21 @@ import keygen
  
 def generate(length=1024):
     p,q,n,t,e,d = keygen.keygen(length)
+    with open('values.txt', "w") as f:
+        f.write(str(n)+"\n"+str(e)+"\n"+str(d))
     return n,e,d
-def encrypt(plaintext, e, n):
+def encrypt(plaintext):
+    with open('values.txt',"r") as f:
+        ned = f.readlines()
+        n = int(ned[0].replace("\n",""))
+        e = int(ned[1].replace("\n",""))
+        d = int(ned[2].replace("\n",""))
     encrypted = pow(plaintext, e , n)
     return encrypted
-def decrypt(ciphertext, d, n):
-    return pow(ciphertext, d, n)   
+def decrypt(ciphertext):
+    with open('values.txt',"r") as f:
+        ned = f.readlines()
+        n = int(ned[0].replace("\n",""))
+        e = int(ned[1].replace("\n",""))
+        d = int(ned[2].replace("\n",""))
+    return pow(ciphertext, d, n)
